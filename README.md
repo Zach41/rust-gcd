55068900148b5fda4325f95300ff2b33b5fec676
Zach41 <zach_41@163.com>
2016-12-12 20:03:36 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# rust-gcd
+
+A threadpool implemention which provides a set of apis similar to grand center dispatch. It eases the management of concurrent tasks.
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! This crate provides a set of apis similiar to grand center dispatch, it
+//! eases the management of concurrent tasks.
+
+#![warn(missing_docs)]
+
+mod gcd;
+mod threadpool;
+
+
+
+
